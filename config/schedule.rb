8e682e38cdf0705b9fdc28edf130a5021c062b72
Bill Cromie <bill@cromie.org>
2017-04-21 18:09:45 -0400

    fixing whenever timezones. again.

--- config/schedule.rb
+++ config/schedule.rb
@@ -8,8 +8,10 @@ require 'yaml'
 require 'time'
 require 'tzinfo'
 
-env_file = File.dirname(__FILE__) + '/local_env.yml'
-defaults = File.dirname(__FILE__) + '/sample.local_env.yml'
+path = File.expand_path(File.dirname(File.dirname(__FILE__)))
+
+env_file = "#{path}/config/local_env.yml"
+defaults = "#{path}/config/sample.local_env.yml"
 
 YAML.load(File.open(env_file)).each do |key, value|
   ENV[key.to_s] = value
@@ -19,8 +21,6 @@ end if File.exist?(env_file)
 YAML.load(File.open(defaults)).each do |key, value|
   ENV[key.to_s] = value unless ENV[key]
 end
-
-path = File.expand_path(File.dirname(File.dirname(__FILE__)))
 
 # run our jobs in the right time zone
 set :job_template, "TZ=\"#{ENV['TIME_ZONE']}\" bash -l -c ':job'"
@@ -34,22 +34,23 @@ end
 # https://coderwall.com/p/ahdolq/local-timezone-fix-for-whenever-gem
 # see: https://github.com/javan/whenever/pull/239
 # time should be > 03:00
-def nyc_time(time)
-  TZInfo::Timezone.get('America/New_York').local_to_utc(Time.parse(time))
+def local_time(time)
+  TZInfo::Timezone.get(ENV['TIME_ZONE']).local_to_utc(Time.parse(time))
 end
 
 # this queues up all the email/sms for the day!
-every :day, at: nyc_time("8:00am") do
+every :day, at: local_time("8:00am") do
   runner "User.send_all_reminders"
   runner "Person.send_all_reminders"
 end
 
-every :day, at: nyc_time("2:00am") do
+every :day, at: local_time("2:00am") do
   command "cd #{path} && bundle exec #{path}/bin/delayed_job restart"
 end
 
 every :reboot do
   command "cd #{path} && #{path}/bin/unicorn_rails -c config/unicorn.rb -E #{ENV['RAILS_ENV']} -D "
+  command "cd #{path} && bundle exec #{path}/bin/delayed_job start"
 end
 #
 # every 4.days do
