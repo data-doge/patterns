4849994b70dc965200aa775681d98457316a6e30
Bill Cromie <bill@cromie.org>
2017-05-04 19:03:47 -0400

    simplifying the person_mailer

--- app/mailers/person_mailer.rb
+++ app/mailers/person_mailer.rb
@@ -3,31 +3,16 @@ class PersonMailer < ApplicationMailer
   # how to get the groovy "accept/decline" thingy...
   # http://stackoverflow.com/questions/27514552/how-to-get-rsvp-buttons-through-icalendar-gem
   # I beleive we will likely have to have inbound email for it to work
-  def invite(email_address:, invitation:, person:)
-    admin_email = ENV['MAILER_SENDER']
-    @email_address = email_address
-    @invitation = invitation
+  def invite(invitation:, person:)
     @person = person
+    @email_address = @person.email_address
+    @invitation = invitation
 
     attachments['event.ics'] = { mime_type: 'application/ics',
                                  content: generate_ical(invitation) }
 
-    mail(to: email_address,
-         from: admin_email,
+    mail(to: @email_address,
          subject: @invitation.title,
-         content_type: 'multipart/mixed')
-  end
-
-  def notify(email_address:, invitation:)
-    @email_address = email_address
-    @invitation = invitation
-
-    attachments['event.ics'] = { mime_type: 'application/ics',
-                                content: generate_ical(invitation) }
-
-    mail(to: email_address,
-         from: invitation.user.email,
-         subject: 'Interview scheduled',
          content_type: 'multipart/mixed')
   end
 
@@ -35,8 +20,7 @@ class PersonMailer < ApplicationMailer
     @email_address = email_address
     @invitations = invitations
 
-    mail(to: email_address,
-         from: ENV['MAILER_SENDER'],
+    mail(to: @email_address,
          subject: 'Today\'s Interview Reminders',
          content_type: 'multipart/mixed')
   end
@@ -45,8 +29,7 @@ class PersonMailer < ApplicationMailer
     @email_address = email_address
     @invitation = invitation
 
-    mail(to: email_address,
-         from: ENV['MAILER_SENDER'],
+    mail(to: @email_address,
          subject: "Canceled: #{invitation.start_datetime_human}",
          content_type: 'multipart/mixed')
   end
@@ -56,17 +39,11 @@ class PersonMailer < ApplicationMailer
     @invitation = invitation
 
     mail(to: email_address,
-         from: ENV['MAILER_SENDER'],
          subject: "Confirmed: #{invitation.start_datetime_human}",
          content_type: 'multipart/mixed')
   end
 
   private
-
-    # if the email is to the user, don't bcc!
-    def bcc_or_nil(email_address, invitation)
-      invitation.user.email == email_address ?  nil : invitation.user.email
-    end
 
     def generate_ical(invitation)
       cal = Icalendar::Calendar.new
