19210340f8f29ae6ef71e8385cce243bdfd94758
Bill Cromie <bill@cromie.org>
2017-12-01 18:20:51 -0500

    removing the automatic update, because the rapidpro api is broken

--- app/jobs/mailchimp_update_job.rb
+++ app/jobs/mailchimp_update_job.rb
@@ -37,20 +37,21 @@ class MailchimpUpdateJob < Struct.new(:id, :status)
         Rails.logger.fatal("[People->sendToMailChimp] fatal error sending #{person.id} to Mailchimp: #{e.message}")
       end
 
-      HTTParty.post('http://rapidpro.brl.nyc/api/v2/contacts.json',
-        headers: { 'Authorization' => ENV['RAPIDPRO_TOKEN'] },
-        body: { name: person.first_name + ' '+ person.last_name,
-                urns: ["tel:#{person.phone_number}"],
-                language: 'eng',
-                fields: {
-                  first_name: person.first_name,
-                  last_name: person.last_name,
-                  email_address: person.email_address,
-                  zip_code: person.postal_code,
-                  neighborhood: person.neighborhood,
-                  patterns_token: person.token,
-                  patterns_id: person.id
-                }})
+      # HTTParty.post('http://rapidpro.brl.nyc/api/v2/contacts.json',
+      #   headers: { 'Authorization' => ENV['RAPIDPRO_TOKEN'] },
+      #   body: { name: person.first_name + ' '+ person.last_name,
+      #           urns: ["tel:#{person.phone_number}"],
+      #           language: 'eng',
+      #           groups:[],
+      #           fields: {
+      #             first_name: person.first_name,
+      #             last_name: person.last_name,
+      #             email_address: person.email_address,
+      #             zip_code: person.postal_code,
+      #             neighborhood: person.neighborhood,
+      #             patterns_token: person.token,
+      #             patterns_id: person.id
+      #           }}.to_json)
 
     end
   end
