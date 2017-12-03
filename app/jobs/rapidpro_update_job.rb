afbda293b9633d78f8aac8dbb062ac8879cbb021
Bill Cromie <bill@cromie.org>
2017-12-03 18:04:21 -0500

    simplyfying rapidpro updates to only deal with folks with phone numbers

--- app/jobs/rapidpro_update_job.rb
+++ app/jobs/rapidpro_update_job.rb
@@ -16,46 +16,48 @@ class RapidproUpdateJob < Struct.new(:id)
   # otherwise use uuid. this will allow changes to phone numbers.
   # additionally, it means we only need one worker.
   def perform
-    person = Person.where(id: id).where.not(phone_number: nil).first
-    return if person.nil? && person.phone_number.blank?
+    person = Person.find(id)
+    # we may deal with a word where rapidpro does email...
+    # but not now.
+    if person.phone_number.present?
+      base_url = 'https://rapidpro.brl.nyc/api/v2/contacts.json'
 
-    base_url = 'https://rapidpro.brl.nyc/api/v2/contacts.json'
+      body = { name: person.full_name, language: 'eng', groups: [], fields: {} }
+      # eventual fields: # first_name: person.first_name,
+      # last_name: person.last_name,
+      # email_address: person.email_address,
+      # zip_code: person.postal_code,
+      # neighborhood: person.neighborhood,
+      # patterns_token: person.token,
+      # patterns_id: person.id
 
-    body = { name: person.full_name, language: 'eng', groups: [], fields: {} }
-    # eventual fields: # first_name: person.first_name,
-    # last_name: person.last_name,
-    # email_address: person.email_address,
-    # zip_code: person.postal_code,
-    # neighborhood: person.neighborhood,
-    # patterns_token: person.token,
-    # patterns_id: person.id
+      urn = "tel:#{person.phone_number}"
 
-    urn = "tel:#{person.phone_number}"
-
-    if person&.rapidpro_uuid.present? # already created in rapidpro
-      url = base_url + "?uuid=#{person.rapidpro_uuid}"
-      body[:urns] = [urn] # adds new phone number if need be.
-      body[:urns] << "mailto:#{person.email_address}" if person.email_address.present?
-    else # person doesn't yet exist in rapidpro
-      cgi_urn = CGI::escape(urn)
-      url = base_url + "?urn=#{cgi_urn}" # uses phone number to identify.
-    end
-
-    headers = { 'Authorization' => "Token #{ENV['RAPIDPRO_TOKEN']}",
-                'Content-Type'  => 'application/json' }
-
-    res = HTTParty.post(url, headers: headers, body: body.to_json)
-
-    case res.code
-    when 201 || 204 || 200 # new person in rapidpro
-      if person.rapidpro_uuid.blank?
-        # update column to skip callbacks
-        person.update_column(:rapidpro_uuid, res.parsed_response['uuid'])
+      if person&.rapidpro_uuid.present? # already created in rapidpro
+        url = base_url + "?uuid=#{person.rapidpro_uuid}"
+        body[:urns] = [urn] # adds new phone number if need be.
+        body[:urns] << "mailto:#{person.email_address}" if person.email_address.present?
+      else # person doesn't yet exist in rapidpro
+        cgi_urn = CGI::escape(urn)
+        url = base_url + "?urn=#{cgi_urn}" # uses phone number to identify.
       end
-    when 429 # throttled
-      self.retry_delay = res.headers['retry-after'].to_i
-    else
-      raise 'error'
+
+      headers = { 'Authorization' => "Token #{ENV['RAPIDPRO_TOKEN']}",
+                  'Content-Type'  => 'application/json' }
+
+      res = HTTParty.post(url, headers: headers, body: body.to_json)
+
+      case res.code
+      when 201 || 204 || 200 # new person in rapidpro
+        if person.rapidpro_uuid.blank?
+          # update column to skip callbacks
+          person.update_column(:rapidpro_uuid, res.parsed_response['uuid'])
+        end
+      when 429 # throttled
+        self.retry_delay = res.headers['retry-after'].to_i
+      else
+        raise 'error'
+      end
     end
   end
 
