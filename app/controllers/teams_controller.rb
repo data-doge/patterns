b7829cbc8069181331d6dd9232d0c1295cd53291
Bill Cromie <bill@cromie.org>
2018-01-10 16:29:38 -0500

    handling js requests for best in place updates

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -57,9 +57,11 @@ class TeamsController < ApplicationController
       if @team.update(team_params)
         format.html { redirect_to @team, notice: 'Team was successfully updated.' }
         format.json { render :show, status: :ok, location: @team }
+        format.js
       else
         format.html { render :edit }
         format.json { render json: @team.errors, status: :unprocessable_entity }
+        format.js
       end
     end
   end
@@ -71,10 +73,12 @@ class TeamsController < ApplicationController
     respond_to do |format|
       format.html { redirect_to teams_url, notice: 'Team was successfully destroyed.' }
       format.json { head :no_content }
+      format.js { head :no_content }
     end
   end
 
   private
+
     # Use callbacks to share common setup or constraints between actions.
     def set_team
       redirect_to root_url unless @current_user.admin? #admin only
