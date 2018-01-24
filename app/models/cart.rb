64dccb706bda9cb3f3d2aad9e1e295e92d50cac3
Bill Cromie <bill@cromie.org>
2018-01-24 15:59:50 -0500

    adding join tables for carts, (read pools) so that we can have multiple users participate in a cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,28 +1,25 @@
 # frozen_string_literal: true
 
-class Cart < ActiveRecord::Base # should be renamed to pool...
+# should be renamed to pool...
+class Cart < ActiveRecord::Base
   belongs_to :user
-  # generally speaking, this is not the right way to do things
-  # however, I *think* it makes sense in the context.
-  # perhaps store is better than serialize, not sure.
-  serialize :people_ids, JSON # it's a string, so 255 char max
+
+  has_many :carts_people, dependent: :destroy
+  has_many :carts_users, dependent: :destroy
+
+  has_many :users, through: :carts_users, dependent: :destroy
+  has_many :people, through: :carts_people, dependent: :destroy
+
+  has_many :comments, as: :commentable, dependent: :destroy
 
   # example validation, the before_save obviates this.
   # validate :uniqueness_of_people_ids
-  before_save :dedupe_people_ids
+  # before_save :dedupe_people_ids
 
-  validates :name, uniqueness: { scope: :user_id,
-                                 message: 'Pools must have a unique name' }
+  validates :name, uniqueness: true
 
-  private
+  def owner
+    user
+  end
 
-    def dedupe_people_ids
-      people_ids.uniq!
-    end
-
-    def uniqueness_of_people_ids
-      if people_ids.detect { |e| people_ids.rindex(e) != people_ids.index(e) }
-        errors.add(:people_ids, 'duplicate person in pool')
-      end
-    end
 end
