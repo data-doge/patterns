2459b8f6f4ea9fe7a660ec3aed5ec2fd58260787
Bill Cromie <bill@cromie.org>
2018-02-26 14:52:29 -0500

    removing pre-existing index :

--- db/migrate/20180214162237_add_indices_for_cart_relationships.rb
+++ db/migrate/20180214162237_add_indices_for_cart_relationships.rb
@@ -1,6 +1,6 @@
 class AddIndicesForCartRelationships < ActiveRecord::Migration[5.1]
   def change
-    add_index :carts_users, [:user_id, :cart_id], unique: true
+    #add_index :carts_users, [:user_id, :cart_id], unique: true
     add_index :carts_people, [:person_id, :cart_id], unique: true
   end
 end
