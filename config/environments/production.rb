f0dba596d92ccfc07034eca219fc3822c642f2f7
Bill Cromie <bill@cromie.org>
2018-05-08 14:43:30 -0400

    perhaps forcing ssl off

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Logan::Application.configure do
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  config.force_ssl = false
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
