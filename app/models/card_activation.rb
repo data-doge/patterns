e0b547ae29d9dd19e8eaa854805e027457e42449
Bill Cromie <bill@cromie.org>
2018-05-08 19:21:09 -0400

    logic for ovveride was incorrect

--- app/models/card_activation.rb
+++ app/models/card_activation.rb
@@ -126,7 +126,7 @@ class CardActivation < ApplicationRecord
 
   # override allows manual check calls
   def create_check_call(override: false)
-    if !override && activation_calls.where(call_type: 'check').size < 5
+    if override || activation_calls.where(call_type: 'check').size < 5
       ActivationCall.create(card_activation_id: id, call_type: 'check')
     end
   end
