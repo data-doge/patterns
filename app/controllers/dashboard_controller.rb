4b48cc10c6aab52d1ff72f1276086e3f81037c98
Bill Cromie <bill@cromie.org>
2018-06-05 13:13:59 -0400

    faster dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -23,18 +23,18 @@ class DashboardController < ApplicationController
     @six_month_gc = GiftCard.where('created_at > ?', 6.months.ago).sum(&:amount)
     @twelve_month_gc = GiftCard.where('created_at > ?', 1.year.ago).sum(&:amount)
 
-    @one_month_people = GiftCard.select('distinct person_id').where('created_at > ?', 1.month.ago).size
-    @six_month_people = GiftCard.select('distinct person_id').where('created_at > ?', 6.months.ago).size
-    @twelve_month_people = GiftCard.select('distinct person_id').where('created_at > ?', 1.year.ago).size
+    @one_month_people = GiftCard.includes(:person).select('distinct person_id').where('created_at > ?', 1.month.ago).size
+    @six_month_people = GiftCard.includes(:person).select('distinct person_id').where('created_at > ?', 6.months.ago).size
+    @twelve_month_people = GiftCard.includes(:person).select('distinct person_id').where('created_at > ?', 1.year.ago).size
 
-    @popular_tags   = Person.active.tag_counts.order('taggings_count DESC').limit(10)
-    @new_tags       = Person.active.tag_counts.order('id desc').limit(10)
+    @popular_tags   = Person.includes(:taggings).active.tag_counts.order('taggings_count DESC').limit(10)
+    @new_tags       = Person.includes(:taggings).active.tag_counts.order('id desc').limit(10)
 
-    @recent_participants   = ResearchSession.order('created_at DESC').where('start_datetime < ?', Time.current).limit(10).map(&:people).flatten.uniq
-    @recent_sessions   = ResearchSession.order('created_at DESC').limit(10)
+    @recent_participants   = ResearchSession.includes(:invitations).order('created_at DESC').where('start_datetime < ?', Time.current).limit(10).map(&:people).flatten.uniq
+    @recent_sessions   = ResearchSession.includes(:invitations).order('created_at DESC').limit(10)
 
-    @upcoming_participants = ResearchSession.where('start_datetime between NOW() and ?', 1.week.from_now).map(&:people).flatten.uniq
-    @upcoming_sessions = ResearchSession.where('start_datetime between NOW() and ?', 1.week.from_now)
+    @upcoming_participants = ResearchSession.includes(:invitations).where('start_datetime between NOW() and ?', 1.week.from_now).map(&:people).flatten.uniq
+    @upcoming_sessions = ResearchSession.includes(:invitations).where('start_datetime between NOW() and ?', 1.week.from_now)
     @cart_people = CartsPerson.includes(:cart,:person).all.limit(20)
   end
 
