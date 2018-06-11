6707d800f741102263b6356455297711d1a37ec9
Bill Cromie <bill@cromie.org>
2018-06-11 17:41:57 -0400

    ngrok cors for tags etc. and also lnks for research session tags

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,6 +25,10 @@ Logan::Application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
+  config.action_dispatch.default_headers = {
+    'Access-Control-Allow-Origin' => '*',
+    'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
