fce115579252c9ddee45bda48f37395471df3058
Bill Cromie <bill@cromie.org>
2018-06-15 17:05:31 -0400

    removing invitable

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,8 +31,7 @@ class User < ApplicationRecord
   has_paper_trail ignore: [:last_sign_in_at]
   # acts_as_tagger #if we want owned tags.
 
-  devise :invitable, 
-         :database_authenticatable,
+  devise :database_authenticatable,
          :registerable,
          :recoverable,
          :rememberable,
