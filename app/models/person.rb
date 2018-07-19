8a4f4f0ece67386e7701c53e0d974e693999d515
Bill Cromie <bill@cromie.org>
2018-07-19 11:54:19 -0400

    showing the last research session in amount admin page

--- app/models/person.rb
+++ app/models/person.rb
@@ -113,7 +113,7 @@ class Person < ApplicationRecord
   scope :active, -> { where(active: true) }
   scope :deactivated, -> { where(active: false) }
 
-  scope :order_by_giftcard_sum, -> {joins(:gift_cards).where('gift_cards.created_at >= ?',Time.current.beginning_of_year).select('people.id, people.first_name,people.last_name, people.active,sum(gift_cards.amount_cents) as total_gc').group('people.id').order('total_gc desc')}
+  scope :order_by_giftcard_sum, -> {joins(:gift_cards).includes(:research_sessions).where('gift_cards.created_at >= ?',Time.current.beginning_of_year).select('people.id, people.first_name,people.last_name, people.active,sum(gift_cards.amount_cents) as total_gc').group('people.id').order('total_gc desc')}
   # no longer using this. now managing active elsewhere
   # default_scope { where(active: 
 
