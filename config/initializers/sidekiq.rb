c1d0ad65964a37a5d482371ed80a51462d19474e
Bill Cromie <bill@cromie.org>
2018-09-10 13:25:47 -0400

    setting sidekiq connection pool using env

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,11 +2,13 @@ redis_conn = proc {
   Redis.current # do anything you want here
 }
 
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 25 }
+
 Sidekiq.configure_server do |config|
-  config.redis = ConnectionPool.new(size: 30, &redis_conn)
+  config.redis = ConnectionPool.new(size: threads_count + 2 , &redis_conn)
   config.average_scheduled_poll_interval = 1 # poll every second
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = ConnectionPool.new(size: 5, &redis_conn)
+  config.redis = ConnectionPool.new(size: threads_count + 2, &redis_conn)
 end
