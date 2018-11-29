193896b009c3646c7a2eee44c41c7da0ebba8381
Bill Cromie <bill@cromie.org>
2018-11-29 09:50:45 -0500

    bumping ruby version

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 set :application, 'logan-production'
 set :branch, fetch(:branch, ENV['PRODUCTION_BRANCH'])
 # use the same ruby as used locally for deployment
-set :rvm_ruby_string, '2.5.1@production'
+set :rvm_ruby_string, '2.5.3@production'
 
 server ENV['PRODUCTION_SERVER'], :app, :web, :db, primary: true
