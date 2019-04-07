78e64cc26c93717f319e1dc4a3f45993a2d3082a
data-doge <eugene@enspiral.com>
2019-04-07 15:23:18 -0400

    jesus christ. so these tests, including most of the commented out tests, were working great until i set js: true. when i did this, capybara was unable to find the .cart-size element. it turned out that it was not visible (to capybara). this is confusing, because it appears to be rendered synchronously, by the server, in application.html.erb. anyways, i learned that you can `find` elements that are not visible, by passing `visible: false` to the `find` method. however, the `has_content?` assertion fails, because the text is assumed to be "" by capybara, for in-visible elements. pretty fucking annoying lol. a way around this, is to call `.text(:all)` (the `:all` is absolutely necessary), on the element.

---
+++ spec/features/pool_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+feature "pools" do
+  let(:admin_user) { FactoryBot.create(:user, :admin) }
+  let(:current_pool) { admin_user.current_cart }
+
+  before do
+    login_with_admin_user(admin_user)
+  end
+
+  scenario "add person to pool", js: true do
+    person = FactoryBot.create(:person)
+    visit people_path
+    expect(page).to have_content(person.email_address)
+    expect(page.find('.badge.cart-size', visible: false).text(:all)).to eq("0")
+
+
+    # expect(page.find('#pool-list').find(:xpath, ".//a[@href='#{cart_path(current_pool)}']")).to have_content("0")
+    #
+    # add_btn = page.find("#person-#{person.id}").find(:xpath, ".//a[@href='#{add_person_cart_index_path(person_id: person.id)}']")
+    # expect(add_btn).to have_content("Add")
+    # add_btn.click
+    # expect(page).to have_content("1 people added to #{current_pool.name}-pool")
+    # expect(page.find('.badge.cart-size')).to have_content("1")
+    # expect(page.find('#pool-list').find(:xpath, ".//a[@href='#{cart_path(current_pool)}']")).to have_content("1")
+    #
+    # delete_btn = page.find("#person-#{person.id}").find(:xpath, ".//a[@href='#{delete_person_cart_index_path(person_id: person.id)}']")
+    # expect(delete_btn).to have_content("Remove")
+
+
+      # flash message
+      # pool (1)
+    # go to pool page
+      # size 1
+      # created by admin_user
+      # sync to radidpro no
+      # users
+        # contains admin
+      # person email exists in list
+
+    # click remove
+      # add btn shown
+      # flash message
+      # pool (0)
+    # go to pool page
+      # size 0
+      # email not present
+      # all the rest same
+  end
+end
