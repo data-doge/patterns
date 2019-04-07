4af4da5f5c052f9fa52331ef813a5726c1848ae2
data-doge <eugene@enspiral.com>
2019-04-07 15:48:49 -0400

    test that add to pool btn works

--- spec/features/pool_spec.rb
+++ spec/features/pool_spec.rb
@@ -13,17 +13,15 @@ feature "pools" do
     visit people_path
     expect(page).to have_content(person.email_address)
     expect(page.find('.badge.cart-size').text).to have_content("0")
+    expect(page.find('#pool-list').find(:xpath, ".//a[@href='#{cart_path(current_pool)}']")).to have_content("0")
+    add_btn = page.find("#person-#{person.id}").find(:xpath, ".//a[@href='#{add_person_cart_index_path(person_id: person.id)}']")
+    expect(add_btn).to have_content("Add")
+    add_btn.click
+    wait_for_ajax
+    expect(page).to have_content("1 people added to #{current_pool.name}")
 
-
-    # expect(page.find('#pool-list').find(:xpath, ".//a[@href='#{cart_path(current_pool)}']")).to have_content("0")
-    #
-    # add_btn = page.find("#person-#{person.id}").find(:xpath, ".//a[@href='#{add_person_cart_index_path(person_id: person.id)}']")
-    # expect(add_btn).to have_content("Add")
-    # add_btn.click
-    # expect(page).to have_content("1 people added to #{current_pool.name}-pool")
     # expect(page.find('.badge.cart-size')).to have_content("1")
     # expect(page.find('#pool-list').find(:xpath, ".//a[@href='#{cart_path(current_pool)}']")).to have_content("1")
-    #
     # delete_btn = page.find("#person-#{person.id}").find(:xpath, ".//a[@href='#{delete_person_cart_index_path(person_id: person.id)}']")
     # expect(delete_btn).to have_content("Remove")
 
