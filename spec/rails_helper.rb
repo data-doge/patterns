8a5586581577549b16249ccffc97745cb01416b0
Bill Cromie <bill@cromie.org>
2019-04-10 14:51:10 -0400

    output slow tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -102,5 +102,7 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
+  config.profile_examples = true
+  
   config.include Devise::Test::ControllerHelpers, :type => :controller
 end
