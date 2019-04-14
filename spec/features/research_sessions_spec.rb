35f6569bd833eaf242ee5c9e270917923cd6c2a4
data-doge <eugene@enspiral.com>
2019-04-14 15:54:39 -0400

    test research sessions attrs persisted

--- spec/features/research_sessions_spec.rb
+++ spec/features/research_sessions_spec.rb
@@ -13,7 +13,7 @@ feature "research sessions" do
     title = "fake title"
     location = "BRL"
     description = "fake description"
-    date = DateTime.current + 2.days
+    start_datetime = Time.zone.now.beginning_of_minute + 2.days
     duration = ResearchSession::DURATION_OPTIONS.first
 
     # create new session
@@ -33,13 +33,23 @@ feature "research sessions" do
     fill_in 'Session Title', with: title
     fill_in 'Session Location', with: location
     fill_in 'Session description', with: description
-    fill_in 'Start datetime', with: date.strftime('%Y-%m-%d %H:%M %p')
+    fill_in 'Start datetime', with: start_datetime.strftime('%Y-%m-%d %H:%M %p')
     select duration, from: 'research_session_duration'
     click_button 'Create'
 
     # verify created correctly
     new_research_session = ResearchSession.order(:id).last
     expect(page.current_path).to eq(research_session_path(new_research_session))
+    expect(new_research_session.title).to eq(title)
+    expect(new_research_session.location).to eq(location)
+    expect(new_research_session.description).to eq(description)
+    expect(new_research_session.start_datetime).to be_within(1.seconds).of(start_datetime)
+    expect(new_research_session.end_datetime).to be_within(1.seconds).of(start_datetime + duration.minutes)
+    expect(new_research_session.duration).to eq(duration)
+
     # in session list
+
+    # test location blank
+
   end
 end
