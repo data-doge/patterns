09f060c46558863a17339174eb6bc421968835e3
Bill Cromie <bill@cromie.org>
2019-04-17 10:56:10 -0400

    oops, comments controller is definitely used

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,98 +1,94 @@
 # frozen_string_literal: true
 
-# # frozen_string_literal: true
+# == Schema Information
+#
+# Table name: comments
+#
+#  id               :integer          not null, primary key
+#  content          :text(65535)
+#  user_id          :integer
+#  commentable_type :string(255)
+#  commentable_id   :integer
+#  created_at       :datetime
+#  updated_at       :datetime
+#  created_by       :integer
+#
 
-# # == Schema Information
-# #
-# # Table name: comments
-# #
-# #  id               :integer          not null, primary key
-# #  content          :text(65535)
-# #  user_id          :integer
-# #  commentable_type :string(255)
-# #  commentable_id   :integer
-# #  created_at       :datetime
-# #  updated_at       :datetime
-# #  created_by       :integer
-# #
+class CommentsController < ApplicationController
 
-# unused
+  before_action :set_comment, only: %i[show edit update destroy]
 
-# class CommentsController < ApplicationController
+  # GET /comments
+  # GET /comments.json
+  def index
+    @comments = Comment.all
+  end
 
-#   before_action :set_comment, only: %i[show edit update destroy]
+  # GET /comments/1
+  # GET /comments/1.json
+  def show; end
 
-#   # GET /comments
-#   # GET /comments.json
-#   def index
-#     @comments = Comment.all
-#   end
+  # GET /comments/new
+  def new
+    @comment = Comment.new
+  end
 
-#   # GET /comments/1
-#   # GET /comments/1.json
-#   def show; end
+  # GET /comments/1/edit
+  def edit; end
 
-#   # GET /comments/new
-#   def new
-#     @comment = Comment.new
-#   end
+  # POST /comments
+  # POST /comments.json
+  def create
+    @comment = Comment.new(comment_params)
 
-#   # GET /comments/1/edit
-#   def edit; end
+    respond_to do |format|
+      if @comment.with_user(current_user).save
+        # format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.js   {}
+        # format.json { render action: 'show', status: :created, location: @comment }
+      else
+        # format.html { render action: 'new' }
+        format.js   { escape_javascript("console.log('error saving comment: #{comment.errors.inpsect}');") }
+        # format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
-#   # POST /comments
-#   # POST /comments.json
-#   def create
-#     @comment = Comment.new(comment_params)
+  # PATCH/PUT /comments/1
+  # PATCH/PUT /comments/1.json
+  def update
+    respond_to do |format|
+      if @comment.with_user(current_user).update(comment_params)
+        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
-#     respond_to do |format|
-#       if @comment.with_user(current_user).save
-#         # format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-#         format.js   {}
-#         # format.json { render action: 'show', status: :created, location: @comment }
-#       else
-#         # format.html { render action: 'new' }
-#         format.js   { escape_javascript("console.log('error saving comment: #{comment.errors.inpsect}');") }
-#         # format.json { render json: @comment.errors, status: :unprocessable_entity }
-#       end
-#     end
-#   end
+  # DELETE /comments/1
+  # DELETE /comments/1.json
+  def destroy
+    @comment.destroy
+    respond_to do |format|
+      # format.html { redirect_to comments_url }
+      # format.json { head :no_content }
+      format.js {}
+    end
+  end
 
-#   # PATCH/PUT /comments/1
-#   # PATCH/PUT /comments/1.json
-#   def update
-#     respond_to do |format|
-#       if @comment.with_user(current_user).update(comment_params)
-#         format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
-#         format.json { head :no_content }
-#       else
-#         format.html { render action: 'edit' }
-#         format.json { render json: @comment.errors, status: :unprocessable_entity }
-#       end
-#     end
-#   end
+  private
 
-#   # DELETE /comments/1
-#   # DELETE /comments/1.json
-#   def destroy
-#     @comment.destroy
-#     respond_to do |format|
-#       # format.html { redirect_to comments_url }
-#       # format.json { head :no_content }
-#       format.js {}
-#     end
-#   end
+    # Use callbacks to share common setup or constraints between actions.
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
 
-#   private
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def comment_params
+      params.require(:comment).permit(:content, :user_id, :commentable_type, :commentable_id)
+    end
 
-#     # Use callbacks to share common setup or constraints between actions.
-#     def set_comment
-#       @comment = Comment.find(params[:id])
-#     end
-
-#     # Never trust parameters from the scary internet, only allow the white list through.
-#     def comment_params
-#       params.require(:comment).permit(:content, :user_id, :commentable_type, :commentable_id)
-#     end
-
-# end
+end
