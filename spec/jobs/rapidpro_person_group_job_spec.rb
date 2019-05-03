044ea82b8bab0a3544c0ec16fc49829d03c2da46
data-doge <eugene@enspiral.com>
2019-05-03 08:25:31 -0400

    testing that `RapidproPersonGroupJob` behaves correctly when the rate-limit is hit. in order to test that the right subsets of people are being iterated through, or queued to be retried later, i had to throw in some `.order`s and `.sort`s throughout the job, and the assoicated spec. i don't see a way around this.

--- spec/jobs/rapidpro_person_group_job_spec.rb
+++ spec/jobs/rapidpro_person_group_job_spec.rb
@@ -2,16 +2,19 @@ require "rails_helper"
 
 RSpec.describe RapidproPersonGroupJob, :type => :job do
   let(:sut) { RapidproPersonGroupJob }
-  let(:people) { FactoryBot.create_list(:person, 110) }
-  let(:rapidpro_uuid) { "fake_uuid" }
-  let(:cart) { FactoryBot.create(:cart, rapidpro_sync: true, rapidpro_uuid: rapidpro_uuid) }
+  let(:people) do
+    _p = FactoryBot.create_list(:person, 110, :rapidpro_syncable)
+    _p.sort_by(&:id)
+  end
+  let(:cart) { FactoryBot.create(:cart, rapidpro_sync: true, rapidpro_uuid: SecureRandom.uuid) }
+  let(:rapidpro_409_res) { Hashie::Mash.new({ code: 429, headers: { 'retry-after': 100 } }) }
 
   def perform_job(action)
-    sut.new.perform(people.pluck(:id), cart.id, action)
+    sut.new.perform(people.map(&:id), cart.id, action)
   end
 
   before do
-    allow(HTTParty).to receive(:post)
+    allow(HTTParty).to receive(:post).and_return(rapidpro_409_res)
   end
 
   context "rapidpro sync is false for cart" do
@@ -39,6 +42,23 @@ RSpec.describe RapidproPersonGroupJob, :type => :job do
 
   context "rapidpro returns status 429" do
     it "enqueues job to be re-run later, with remaining people" do
+      action = "add"
+      last_100 = people.last(100)
+      first_10 = people.first(10)
+      request_url = "https://rapidpro.brl.nyc/api/v2/contact_actions.json"
+      request_headers = {
+        'Authorization' => "Token #{ENV['RAPIDPRO_TOKEN']}",
+        'Content-Type'  => 'application/json'
+      }
+      request_body = {
+        action: action,
+        contacts: last_100.map(&:rapidpro_uuid),
+        group: cart.rapidpro_uuid
+      }
+
+      expect(HTTParty).to receive(:post).with(request_url, headers: request_headers, body: request_body.to_json)
+      expect_any_instance_of(sut).to receive(:retry_later).with(first_10.map(&:id), 105)
+      perform_job(action)
     end
   end
 
